d453bc5f023d8dab4277e0b38ff90c1c9b23de1c
吴威 <664507439@qq.com>
2020-02-25 21:00:32 +0800

    no message

--- start.sh
+++ start.sh
@@ -20,4 +20,4 @@ then
     /usr/bin/docker rm "$word"
 fi
  mvn clean package -P spring-boot docker:build
- run -p 8761:8761 -d --name "$appName" "$appName":latest
+ docker run -p 8761:8761 -d --name "$appName" "$appName":latest
