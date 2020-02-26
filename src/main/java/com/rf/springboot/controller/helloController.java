7bec2da2869592fd50e636932d474b80014273cc
吴威 <664507439@qq.com>
2020-02-26 17:04:47 +0800

    no message

--- src/main/java/com/rf/springboot/controller/helloController.java
+++ src/main/java/com/rf/springboot/controller/helloController.java
@@ -1,5 +1,7 @@
 package com.rf.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class helloController {
+
+    public static final Logger logger = LoggerFactory.getLogger(helloController.class);
+
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
