ee3b94a3b02c0e082f619861b0f9be3e11dc71d2
吴威 <664507439@qq.com>
2020-02-29 00:39:52 +0800

    测试超时熔断

--- src/main/java/com/rf/springboot/controller/helloController.java
+++ src/main/java/com/rf/springboot/controller/helloController.java
@@ -20,4 +20,15 @@ public class helloController {
         return "hello";
     }
 
+    @RequestMapping("/timeout")
+    public String timeout(){
+        try{
+            //睡5秒，网关Hystrix3秒超时，会触发熔断降级操作
+            Thread.sleep(6000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "timeout";
+    }
+
 }
